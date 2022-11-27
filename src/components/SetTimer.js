b8ff84ae93ae3234e91de0d7b8ff93a488fe870d
ManjiCoder <manjicoder@gmail.com>
2022-11-28 02:17:52 +0530

    Icons add

--- src/components/SetTimer.js
+++ src/components/SetTimer.js
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 
 function SetTimer() {
   const [date, setDate] = useState("");
+  const [note, setNote] = useState("");
   const [todayDate, setTodayDate] = useState("");
   const [dateArr, setDateArr] = useState([]);
   const [time, setTime] = useState(false);
@@ -24,6 +25,7 @@ function SetTimer() {
   const addDate = () => {
     console.log(date);
     let userSelectedDate = new Date(date);
+    let userTargetDate = userSelectedDate;
     userSelectedDate.setHours(
       time ? Number.parseInt(time.toString().slice(0, 2)) : 0
     );
@@ -35,30 +37,47 @@ function SetTimer() {
 
     // console.log(time);
     dateArr.unshift(userSelectedDate.getTime());
+    let data = {
+      date: userTargetDate.toGMTString().slice(0, 17),
+      note: note,
+    };
     localStorage.setItem(
       JSON.stringify(userSelectedDate.getTime()),
-      userSelectedDate
+      JSON.stringify(data)
     );
-    console.log(dateArr);
+    // console.log(dateArr);
     setDate("");
     setTime(false);
-    setCheck(false);
+    setNote("");
+    // setCheck(false);
+  };
+
+  const deleteDate = (id) => {
+    console.log(id);
+  };
+  const editDate = (id) => {
+    let today = new Date(JSON.parse(id));
+    console.log(id, today);
+    let dd = (today.getDate() + (check ? 0 : 1)).toString().padStart(2, 0);
+    let mm = (today.getMonth() + 1).toString().padStart(2, 0);
+    let yy = today.getFullYear().toString().padStart(2, 0);
+    setDate(`${yy}-${mm}-${dd}`);
   };
   useEffect(() => {
     for (let i = 0; i < localStorage.length; i++) {
-      console.log(localStorage.key(i));
+      // console.log(localStorage.key(i));
       dateArr.unshift(localStorage.key(JSON.parse(i)));
     }
     setDateArr(Array.from(new Set(dateArr.sort((a, b) => a - b))));
-    console.log(dateArr);
-    console.log(localStorage);
+    // console.log(dateArr);
+    // console.log(localStorage);
     // eslint-disable-next-line
   }, []);
 
   return (
-    <div className="bg-yellow-400 min-h-[100vh] py-8 px-4">
+    <div className="bg-yellow-500 min-h-[100vh] py-8 px-4">
       <form
-        className="flex flex-col justify-center items-center gap-5"
+        className="flex flex-col justify-center md:items-center gap-5 my-5 items-start w-10/12 m-auto"
         onSubmit={(e) => e.defaultPrevented()}
       >
         <h2 className="text-3xl font-bold">Set The Counter</h2>
@@ -78,6 +97,25 @@ function SetTimer() {
             min={today}
             onChange={(e) => setDate(e.target.value)}
             value={date}
+          />
+        </div>
+
+        {/* Note */}
+        <div className="md:flex justify-center items-center font-semibold text-2xl">
+          <label htmlFor="note">
+            <p>
+              Enter a note <b className="mr-2">:</b>
+            </p>
+          </label>
+
+          <input
+            className="mt-2 md:mt-0 py-2 px-4 rounded-md shadow-md bg-slate-50 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 md:w-[49%] md:ml-1.5"
+            type="text"
+            name=""
+            id="note"
+            onChange={(e) => setNote(e.target.value)}
+            value={note}
+            placeholder="Enter Your Note"
           />
         </div>
 
@@ -132,12 +170,16 @@ function SetTimer() {
       {dateArr.map((date, index) => {
         return (
           <section
+            id={date}
             key={index}
-            className={`h-36  w-full md:max-w-xl lg:max-w-2xl p-2 flex m-auto ${
-              Math.sign(Math.floor(date - todayDate)) == -1 ? "hidden" : ""
+            className={`h-36 md:h-44  w-full md:max-w-xl lg:max-w-2xl p-2 flex m-auto ${
+              Math.sign(Math.floor(date - todayDate)) === -1 ? "hidden" : ""
             }`}
           >
             <div className="bg-cyan-800 w-[35%] flex flex-col items-center justify-center rounded-l-lg">
+              <div className="text-xs md:text-xl font-bold mb-1 text-white">
+                {JSON.parse(localStorage.getItem(date)).date}
+              </div>
               <div className="bg-slate-50 w-[70%] rounded-tr-lg rounded-tl-lg h-6 mb-0.5 text-base font-bold grid place-items-center">
                 Days
               </div>
@@ -155,6 +197,10 @@ function SetTimer() {
             </div>
 
             <div className="bg-slate-800 w-[75%] rounded-r-lg flex items-center justify-center text-whitefont-bold text-white flex-col">
+              <div className="-mt-2 mb-2">
+                <i className="bi-alarm mr-2 text-xl"></i>
+                <span>{JSON.parse(localStorage.getItem(date)).note}</span>
+              </div>
               <div className="flex text-center gap-1">
                 <div>
                   <div
@@ -220,8 +266,24 @@ function SetTimer() {
                     Secs
                   </div>
                 </div>
+                <div className="flex flex-col justify-between">
+                  <button
+                    onClick={() => {
+                      deleteDate(date);
+                    }}
+                  >
+                    <i className="cursor-pointer rounded-full bg-slate-900 shadow-md hover:bg-slate-700 px-2 py-1 md:text-xl bi bi-trash"></i>
+                  </button>
+
+                  <button
+                    onClick={() => {
+                      editDate(date);
+                    }}
+                  >
+                    <i className="cursor-pointer rounded-full bg-slate-900 shadow-md hover:bg-slate-700 px-2 py-1 md:text-xl bi bi-pencil-square"></i>
+                  </button>
+                </div>
               </div>
-              <div className="text-xs mt-2">{localStorage.getItem(date)}</div>
             </div>
           </section>
         );
